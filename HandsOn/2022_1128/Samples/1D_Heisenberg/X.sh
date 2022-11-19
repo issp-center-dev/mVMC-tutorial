cafa9036e9ed905b6ac1b35c8a360bd45ce0a4a9
三澤貴宏 <misawatakahiro@misawatakahironoMacBook-Pro.local>
2022-11-19 20:18:59 +0800

    add sample for 2022 tutorial

---
+++ HandsOn/2022_1128/Samples/1D_Heisenberg/X.sh
@@ -0,0 +1,26 @@
+#[s] definitions of executions
+MPI=" "
+VMC="./vmc.out"
+VMCDRY="./vmcdry.out"
+#[e] definitions of executions
+
+python3 MakeInput.py input.toml
+#[s] opt
+  ${VMCDRY} ./StdFace.def
+  ${MPI} ${VMC} namelist.def 
+  cp ./output/zqp_opt.dat . 
+  mv output opt
+#[e] opt
+
+#[s] aft
+  ${VMCDRY} ./StdFace_aft.def
+  cp green1.def greenone.def 
+  cp green2.def greentwo.def
+  ${MPI} ${VMC} namelist.def ./zqp_opt.dat
+  mv output aft
+#[e] aft
+
+#[s] post process
+  python3 VMClocal.py  input.toml
+  python3 VMCcor.py    input.toml
+#[e] post process
---
+++ HandsOn/2022_1128/Samples/1D_Hubbard/Clean.sh
@@ -0,0 +1,10 @@
+rm *.def
+rm *.dat
+rm *.gp
+rm mVMC.*
+rm -r UHF
+rm -r opt*
+rm -r aft
+rm -r output
+rm -r __pycache__
+
